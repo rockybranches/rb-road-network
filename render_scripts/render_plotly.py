892bd10e7ee36abe6801f9330c8565d4a61d8327
Robbie Capps <rocapp@gmail.com>
2024-06-11 18:30:00 -0700

    fixed notebook and render_plotly.py (both confirmed working now)

--- render_scripts/render_plotly.py
+++ render_scripts/render_plotly.py
@@ -1,17 +1,36 @@
 import pandas as pd
-from urllib.request import urlopen
 import json
-with urlopen('https://robcapps.com/docs/justPopResult2.json') as resp:
-    res = json.load(resp)
-    center_pt = dict(zip(['lon', 'lat'], res['features'][0]['geometry']['coordinates']))
-    coords = res['features'][1]['geometry']['coordinates']
-    df = pd.DataFrame(coords, columns=['lon', 'lat'])
-    # display(df.head())
-    import plotly.express as px
-    fig=px.scatter_mapbox(df, lat='lat', lon='lon', center=center_pt, mapbox_style='open-street-map', zoom=6,
-                                                color_discrete_sequence=['rgba(122, 200, 255, 0.5)'])
-    fig.update_geos(
+
+
+# render_plotly.py:
+#   1. Load the json file
+#   2. Convert the json to a pandas dataframe
+#   3. Plot the dataframe using plotly express
+
+
+def main():
+    with open('/home/robertc/Git/rb-road-network/output/SavannaShoals.json', 'r') as resp:
+        res = json.load(resp)
+        center_pt = dict(zip(['lon', 'lat'], res['features']
+                             [0]['geometry']['coordinates']))
+        coords = res['features'][1]['geometry']['coordinates']
+        df = pd.DataFrame(coords, columns=['lon', 'lat'])
+        # display(df.head())
+        import plotly.express as px
+        fig = px.scatter_mapbox(
+            df,
+            lat='lat', lon='lon', center=center_pt,
+            zoom=6,
+            mapbox_style='open-street-map',
+            color_discrete_sequence=['rgba(122, 200, 255, 0.5)']
+        )
+        fig.update_geos(
             visible=True, scope='usa', showsubunits=True, showcountries=True,
         )
-    fig.add_scattergeo(lat=[center_pt['lat']],lon=[center_pt['lon']], fillcolor='rgba(0, 0, 0, 1)')
-    fig.show()
+        fig.add_scattergeo(lat=[center_pt['lat']], lon=[
+            center_pt['lon']], fillcolor='rgba(0, 0, 0, 1)')
+        fig.show()
+
+
+if __name__ == '__main__':
+    main()
