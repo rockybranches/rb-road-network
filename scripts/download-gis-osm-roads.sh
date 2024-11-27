a53b4c8b9ac7596b57de840db5cb0da75de72209
Robbie Capps <rocapp@gmail.com>
2024-11-27 05:02:27 -0800

    fixed DiggsSouthSand Result

--- scripts/download-gis-osm-roads.sh
+++ scripts/download-gis-osm-roads.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 source ./.envrc
+. ./scripts/_prebuild_setup.sh
 
 echo "RB_SRC=${RB_SRC}"
 echo "RB_DATA=${RB_DATA}"
 
-aria2c --continue -j 10 -d ~/Documents/rb_data/gis_osm_roads_downloads -i $RB_SRC/resources/gis_osm_roads_updates.aria2
+source ./.venv/bin/activate
+
+python -m pip install -r ./requirements.txt
+
+python ./scripts/download-gis-osm-roads.py "$@"
+
+
+
